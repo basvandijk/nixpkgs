9df2054f4b67718f6ca707c241c6d527cb4bdb99
Bas van Dijk <v.dijk.bas@gmail.com>
2020-06-15 18:56:51 +0200

    Some refactoring & included a TODO on cloud-init

--- nixos/modules/virtualisation/maas-image.nix
+++ nixos/modules/virtualisation/maas-image.nix
@@ -45,24 +45,75 @@ in {
       inherit config lib pkgs;
     };
 
-    fileSystems."/" = {
-      device = "/dev/disk/by-label/nixos";
-      autoResize = true;
-      fsType = "ext4";
+    boot = {
+      loader.grub = {
+        version = 2;
+        device = "nodev";
+        efiSupport = true;
+        efiInstallAsRemovable = true;
+      };
+      growPartition = true;
     };
 
-    fileSystems."/boot" = {
-      device = "/dev/disk/by-label/ESP";
-      fsType = "vfat";
+    fileSystems = {
+      "/boot" = {
+        device = "/dev/disk/by-label/ESP";
+        fsType = "vfat";
+      };
+      "/" = {
+        device = "/dev/disk/by-label/nixos";
+        autoResize = true;
+        fsType = "ext4";
+      };
     };
 
-    boot.growPartition = true;
-
-    boot.loader.grub = {
-      version = 2;
-      device = "nodev";
-      efiSupport = true;
-      efiInstallAsRemovable = true;
-    };
+    # TODO: we probably need to enable and configure cloud-init:
+    #services.cloud-init = {
+    #  enable = true;
+    #  config = ''
+    #    system_info:
+    #      distro: nixos
+    #    users:
+    #       - root
+    #
+    #    disable_root: false
+    #    preserve_hostname: false
+    #
+    #    cloud_init_modules:
+    #     - migrator
+    #     - seed_random
+    #     - bootcmd
+    #     - write-files
+    #     - growpart
+    #     - resizefs
+    #     - update_etc_hosts
+    #     - ca-certs
+    #     - rsyslog
+    #     - users-groups
+    #
+    #    cloud_config_modules:
+    #     - disk_setup
+    #     - mounts
+    #     - ssh-import-id
+    #     - set-passwords
+    #     - timezone
+    #     - disable-ec2-metadata
+    #     - runcmd
+    #     - ssh
+    #
+    #    cloud_final_modules:
+    #     - rightscale_userdata
+    #     - scripts-vendor
+    #     - scripts-per-once
+    #     - scripts-per-boot
+    #     - scripts-per-instance
+    #     - scripts-user
+    #     - ssh-authkey-fingerprints
+    #     - keys-to-console
+    #     - phone-home
+    #     - final-message
+    #     - power-state-change
+    #  '';
+    #};
   };
 }
